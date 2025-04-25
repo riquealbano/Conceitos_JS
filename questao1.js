e0464b9b3546f95b56dcdb0f054fac982b17bdb7
Albano <164598919+riquealbano@users.noreply.github.com>
2025-04-24 21:31:18 -0300

    Update questao1.js

--- questao1.js
+++ questao1.js
@@ -8,3 +8,21 @@ console.log(`Soma: ${num1 + num2}`)
 console.log(`Subtração: ${num1 - num2}`)
 console.log(`Multiplicação: ${num1 * num2}`)
 console.log(`Divisão: ${num1 / num2}`)
+
+
+void proximo() {
+    if (primeiro != null) {
+        NoMusicaDuplo temp_no = primeiro;
+        for (int i = 0; i < indice && temp_no != null; i++) {
+            temp_no = temp_no.prox;
+        }
+
+        if (temp_no != null && temp_no.prox != null) {
+            temp_no = temp_no.prox;
+            indice++;
+            System.out.println("Próxima música: " + temp_no.musica.titulo);
+        } else {
+            System.out.println("Não há próximo nó ou já estamos no final da lista.");
+        }
+    }
+}
